55171a99fafc402c170e0ac80826052bb8737ba7
Michael Eze <michaeleze3@gmail.com>
2021-02-10 15:07:05 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
   //uri: 'http://localhost:8080/api',
-  uri: 'https://sapmle-shopping-cart.herokuapp.com/',
+  uri: 'https://sapmle-shopping-cart.herokuapp.com/api',
   cache: new InMemoryCache()
 });
 
